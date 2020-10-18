6c027e42f3a43aa8c810695740c4cfa1d8347bed
Abdullah Alam <abdullahalam598@gmail.com>
2020-10-18 09:15:21 +0530

    Mentions and filters seem to be working

--- main/views.py
+++ main/views.py
@@ -17,11 +17,9 @@ def index(request):
 
 def pickwinner(request):
 
-    if request.method == 'POST':    
+    if request.method == 'POST':
 
-        # print(request.POST)
-        # print(request.method)
-
+        print(request.POST)
 
         # SANITIZE EVERYTHING
         returnthis = ''
@@ -40,16 +38,40 @@ def pickwinner(request):
         if req_mentions_str == '':
             req_mentions = 0
         else:
-            req_mentions = 0
+            req_mentions = int(req_mentions_str)
+            
 
-        # USE IGHELPER
-        ig_helper  = InstagramHelper()
-        all_comments = ig_helper.get_all_comments(url=post_link)
-        random_comment = ig_helper.get_random_comment(all_comments)
+
+        comment_text = ''
+        comment_username = ''
+
+        if len(errors) == 0:
+            # USE IGHELPER
+            ig_helper  = InstagramHelper()
+            all_comments = ig_helper.get_all_comments(url=post_link)
+
+            # FILTER COMMENTS
+            if filter_string != '':
+                all_comments = ig_helper.filter_comments(all_comments, filter_string)
+
+            # CHECK MENTIONS
+            if req_mentions > 0:
+                all_comments = ig_helper.mention_count_filter(all_comments, req_mentions)
+
+            # PICK A RANDOM COMMENT
+            random_comment = ig_helper.get_random_comment(all_comments)
+
+            # SET DATA
+            if random_comment is not None:
+                comment_text = random_comment.text
+                comment_username = random_comment.owner.username
+            else:
+                errors.append('NOCOMMENT')
+        
 
         d = {
-            'comment_text': random_comment.text,
-            'comment_username': random_comment.owner.username,
+            'comment_text': comment_text,
+            'comment_username': comment_username,
             'wait': 30,
             'errors': errors
         }
